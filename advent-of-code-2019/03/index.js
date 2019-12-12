cd23673d20070afccbea5c4a2a8360098e0427b8
harleenquinzell <erika.saadi@gmail.com>
2019-12-12 20:05:56 +0100

    day 3 part 1

--- advent-of-code-2019/03/index.js
+++ advent-of-code-2019/03/index.js
@@ -11,7 +11,7 @@ const getWireCoordinates = wire => wire.split(",");
 const getPath = wire => {
     let x = 0;
     let y = 0;
-    let coordinates = [];
+    let coordinates = new Set();
 
     wire.map(coord => {
         let direction = coord[0];
@@ -21,28 +21,28 @@ const getPath = wire => {
         case "R":
             while (distance > 0) {
                 x += 1;
-                coordinates.push([x, y]);
+                coordinates.add([x, y].toString());
                 distance--;
             }
             break;
         case "L":
             while (distance > 0) {
                 x -= 1;
-                coordinates.push([x, y]);
+                coordinates.add([x, y].toString());
                 distance--;
             }
             break;
         case "U":
             while (distance > 0) {
                 y += 1;
-                coordinates.push([x, y]);
+                coordinates.add([x, y].toString());
                 distance--;
             }
             break;
         case "D":
             while (distance > 0) {
                 y -= 1;
-                coordinates.push([x, y]);
+                coordinates.add([x, y].toString());
                 distance--;
             }
             break;
@@ -56,22 +56,17 @@ const resultFirstPart = input => {
     const [wire1, wire2] = input.split("\n");
 
     const wire1Path = getPath(getWireCoordinates(wire1));
-    console.log("pegou o path do 1", wire1Path)
     const wire2Path = getPath(getWireCoordinates(wire2));
-    console.log("pegou o path do 2", wire2Path)
 
-    const wire1Set = new Set(wire1Path.map(x => x.toString()));
-    const wire2Set = new Set(wire2Path.map(x => x.toString()));
+    const intersections = new Set([...wire1Path].filter(x => wire2Path.has(x)));
 
-    const intersection = new Set([...wire1Set].filter(x => wire2Set.has(x)));
+    const final = [...intersections].map(coord => {
+        const a = coord.split(",");
+        const x = Number(a[0]);
+        const y = Number(a[1]);
 
-    const final = [...intersection].map(xy => {
-        const a = xy.split(",");
-        return Math.abs(Number(a[0])) + Math.abs(Number(a[1]));
+        return Math.abs(x) + Math.abs(y);
     });
-
-    console.log("intersection: ", intersection);
-    console.log(final)
 
     return Math.min(...final);
 };
@@ -82,4 +77,4 @@ console.log("\x1b[32m%s\x1b[0m", "Result of Day 3, part 1 is:", resultFirstPart(
 // Part 2: ------------------------------------------------------------------------------------
 
 // Testing: -----------------------------------------------------------------------------------
-//assert.strictEqual(resultFirstPart("R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83"), 159);
+assert.strictEqual(resultFirstPart("R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83"), 159);
